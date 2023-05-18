4558c7a7e421b5064927bbd667d1be3f8b9d3444
amir-ys <amir.you74@gmail.com>
2023-05-19 00:36:19 +0430

    add Vuejs with vite plugin to project

--- Modules/Core/Providers/ModuleRouteServiceProvider.php
+++ Modules/Core/Providers/ModuleRouteServiceProvider.php
@@ -26,44 +26,44 @@ class ModuleRouteServiceProvider extends ServiceProvider
         parent::boot();
     }
 
-    /**
-     * Define the routes for the application.
-     *
-     * @return void
-     */
-    public function map()
-    {
-        $this->mapApiRoutes();
-
-        $this->mapWebRoutes();
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-            ->namespace($this->moduleNamespace)
-            ->group(module_path('Core', '/Routes/web.php'));
-    }
-
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
-    protected function mapApiRoutes()
-    {
-        Route::prefix('api')
-            ->middleware('api')
-            ->namespace($this->moduleNamespace)
-            ->group(module_path('Core', '/Routes/api.php'));
-    }
+//    /**
+//     * Define the routes for the application.
+//     *
+//     * @return void
+//     */
+//    public function map()
+//    {
+//        $this->mapApiRoutes();
+//
+//        $this->mapWebRoutes();
+//    }
+//
+//    /**
+//     * Define the "web" routes for the application.
+//     *
+//     * These routes all receive session state, CSRF protection, etc.
+//     *
+//     * @return void
+//     */
+//    protected function mapWebRoutes()
+//    {
+//        Route::middleware('web')
+//            ->namespace($this->moduleNamespace)
+//            ->group(module_path('Core', '/Routes/web.php'));
+//    }
+//
+//    /**
+//     * Define the "api" routes for the application.
+//     *
+//     * These routes are typically stateless.
+//     *
+//     * @return void
+//     */
+//    protected function mapApiRoutes()
+//    {
+//        Route::prefix('api')
+//            ->middleware('api')
+//            ->namespace($this->moduleNamespace)
+//            ->group(module_path('Core', '/Routes/api.php'));
+//    }
 }
